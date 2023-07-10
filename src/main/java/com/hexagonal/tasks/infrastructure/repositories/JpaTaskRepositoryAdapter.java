bc89edb4d6e3a0d3d0cfbad2d623f5bed808f26d
drako9159 <chochito9159@gmail.com>
2023-07-10 16:57:00 -0600

    add config to reository

--- src/main/java/com/hexagonal/tasks/infrastructure/repositories/JpaTaskRepositoryAdapter.java
+++ src/main/java/com/hexagonal/tasks/infrastructure/repositories/JpaTaskRepositoryAdapter.java
@@ -1,4 +1,43 @@
 package com.hexagonal.tasks.infrastructure.repositories;
 
-public class JpaTaskRepositoryAdapter {
+import com.hexagonal.tasks.domain.models.Task;
+import com.hexagonal.tasks.domain.ports.out.TaskRepositoryPort;
+import com.hexagonal.tasks.infrastructure.entities.TaskEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JpaTaskRepositoryAdapter implements TaskRepositoryPort {
+    private final JpaTaskRepository jpaTaskRepository;
+
+    public JpaTaskRepositoryAdapter(JpaTaskRepository jpaTaskRepository) {
+        this.jpaTaskRepository = jpaTaskRepository;
+    }
+
+    @Override
+    public Task save(Task task) {
+        TaskEntity taskEntity = TaskEntity.fromDomainModel(task);
+        TaskEntity savedTaskEntity = jpaTaskRepository.save(taskEntity);
+        return savedTaskEntity.toDomainModel();
+    }
+
+    @Override
+    public Optional<Task> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Task> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Task> update(Task task) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        return false;
+    }
 }
